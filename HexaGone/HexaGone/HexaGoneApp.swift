3e3afcd895de65a9638d9485196bc401e43be43a
プ呂バブりー <uncertainquantumduck@gmail.com>
2024-04-23 13:04:55 -0400

    Project Creation
    Created Xcode Project

---
+++ HexaGone/HexaGone/HexaGoneApp.swift
@@ -0,0 +1,15 @@
+//
+//  HexaGoneApp.swift
+//  HexaGone
+//
+
+import SwiftUI
+
+@main
+struct HexaGoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
