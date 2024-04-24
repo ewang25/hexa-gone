835e5e0fd50fa675cad642ecc900c7c536a22a51
プ呂バブりー <uncertainquantumduck@gmail.com>
2024-04-24 11:28:31 -0400

    Create GameView.swift

---
+++ HexaGone/HexaGone/GameView.swift
@@ -0,0 +1,59 @@
+//
+//  GameView.swift
+//  HexaGone
+//
+
+import SwiftUI
+
+struct GameView: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var offset: CGSize = .zero
+    @GestureState private var zoomScale: CGFloat = 1.0
+    @GestureState private var dragOffset: CGSize = .zero
+
+    var zoomAndDrag: some Gesture {
+        SimultaneousGesture(
+            MagnificationGesture()
+                .updating($zoomScale) { value, state, _ in
+                    state = value
+                }
+                .onEnded { value in
+                    self.scale *= value
+                },
+            DragGesture()
+                .updating($dragOffset) { value, state, _ in
+                    state = value.translation
+                }
+                .onEnded { value in
+                    self.offset = CGSize(width: self.offset.width + value.translation.width,
+                                         height: self.offset.height + value.translation.height)
+                }
+        )
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Zoom and drag the game board")
+                .padding()
+            
+            GeometryReader { geometry in
+                self.gameBoard
+                    .frame(width: 300, height: 300)
+                    .background(Color.blue)
+                    .clipShape(Rectangle())
+                    .scaleEffect(scale * zoomScale)
+                    .offset(x: offset.width + dragOffset.width, y: offset.height + dragOffset.height)
+                    .gesture(self.zoomAndDrag)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+    
+    var gameBoard: some View {
+        Text("Testing Here") // Placeholder for the game board
+    }
+}
+
+#Preview {
+    GameView()
+}
