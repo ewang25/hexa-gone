b666e013306fc3f6a3ff9fb25c56086dcd47f9af
プ呂バブりー <uncertainquantumduck@gmail.com>
2024-04-24 12:01:44 -0400

    Create HexBoardView.swift
    Still Testing

---
+++ HexaGone/HexaGone/HexBoardView.swift
@@ -0,0 +1,46 @@
+//
+//  HexBoardView.swift
+//  HexaGone
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct HexagonView: View {
+    var body: some View {
+        Image("tile_1")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50)
+    }
+}
+
+struct HexBoardView: View {
+    var cols = 9
+    var rows = 9
+    let hexSize: CGFloat = 45 // mess around with this
+
+    var body: some View {
+        let hexHeight = sqrt(3.0) / 2 * hexSize
+        let hexWidth = hexSize
+        return GeometryReader { geometry in
+            VStack(spacing: 0) {
+                ForEach(0..<rows, id: \.self) { row in
+                    HStack(spacing: 0) {
+                        ForEach(0..<cols, id: \.self) { col in
+                            HexagonView()
+                                .frame(width: hexWidth, height: hexHeight)
+                                .offset(x: (row % 2 != 0) ? hexWidth / 2 : 0)
+                        }
+                    }
+                    .offset(y: row % 2 == 0 ? -hexHeight / 4 : -hexHeight / 4)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HexBoardView()
+}
