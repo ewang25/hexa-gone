fa12dcaae6a419c8af27ba7870f5d8314e991f79
プ呂バブりー <uncertainquantumduck@gmail.com>
2024-04-25 12:11:44 -0400

    Implemented Timer

--- HexaGone/HexaGone/GameView.swift
+++ HexaGone/HexaGone/GameView.swift
@@ -13,6 +13,30 @@ struct GameView: View {
     
     @State var flagCount = 0
     
+    // TIMER
+    @State private var startTime = Date()
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var elapsedTime = 0
+    
+    // Computes the elapsed time string
+    var elapsedTimeString: String {
+        let totalSeconds = elapsedTime
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+    
+    // Updates the elapsed time based on the current date and the start time
+    func updateElapsedTime() {
+        elapsedTime = Int(Date().timeIntervalSince(startTime))
+    }
+    
+    // Clean up the timer when the view disappears
+    func cleanupTimer() {
+        timer.upstream.connect().cancel()
+    }
+    
     var body: some View {
         ZStack {
             ZoomAndDragView(
@@ -45,11 +69,29 @@ struct GameView: View {
                         }
                     }
                     Spacer()
+                    ZStack {
+                        Rectangle()
+                            .fill(.white)
+                            .cornerRadius(10)
+                            .frame(width: 140, height: 90)
+                            .padding()
+                        VStack (spacing: 0) {
+                            Image(systemName: "timer.circle")
+                                .font(.title)
+                            Text("\(elapsedTimeString)")
+                                .onReceive(timer) { _ in
+                                    updateElapsedTime()
+                                }
+                                .onAppear {
+                                    startTime = Date()  // Reset the start time when the view appears
+                                }
+                        }
+                    }
                 }
                 Spacer()
             }
         }
-        // Custom back button.
+        // Custom back button
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
