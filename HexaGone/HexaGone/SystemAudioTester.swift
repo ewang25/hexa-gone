a13b047ae8b7cc883ac45db1d4bbef5fb811aadd
プ呂バブりー <uncertainquantumduck@gmail.com>
2024-04-29 23:42:37 -0400

    Basic System Sounds Tester (#3)

---
+++ HexaGone/HexaGone/SystemAudioTester.swift
@@ -0,0 +1,37 @@
+//
+//  SystemAudioTester.swift
+//  HexaGone
+//
+
+import SwiftUI
+import AVKit
+
+// This view is meant for testing system audio options to use for our game
+// Find a full list of them here:
+// https://github.com/TUNER88/iOSSystemSoundsLibrary
+// Record down the sounds that you think could work with the game down below:
+//
+// ...
+//
+
+let soundID : SystemSoundID = 1004
+
+struct SystemAudioTester: View {
+    var body: some View {
+        Button(action: {
+            AudioServicesPlaySystemSound(soundID)
+        }) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width:200, height:100)
+                Text("Play Sound")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    SystemAudioTester()
+}
